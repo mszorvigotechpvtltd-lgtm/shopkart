4007daf848ddc7bb681ea0f5a947341cb4c6a97f
mszorvigotechpvtltd-lgtm <mszorvigotechpvt.ltd@gmail.com>
2025-09-04 13:51:27 +0530

    autofixer added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
-// tailwind.config.js
-import { siteTheme } from "@/config/theme"; // use alias if alias is configured
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}", // keep this too if you actually use src/
+  ],
   theme: {
     extend: {
       colors: {
